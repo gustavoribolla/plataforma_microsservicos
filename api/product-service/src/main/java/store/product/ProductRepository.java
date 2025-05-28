e4ff92e93f5f00f339ce24043175abf2ee3ec0e5
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-05-27 22:35:25 -0300

    remove: api/product-service removido do versionamento para virar submódulo

--- api/product-service/src/main/java/store/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package store.product;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<ProductModel, String> {  
-}
