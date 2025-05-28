d33ac72de5bbea48b5540e7faf0520aee45e39a1
ribollequis87 <gustavocr2@al.insper.edu.br>
2025-05-27 22:50:31 -0300

    remove: api/order-service removido do versionamento para virar submódulo

--- api/order-service/src/main/java/store/order/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package store.order;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends CrudRepository<ItemModel, String> {
-    public Iterable<ItemModel> findByIdOrder(String OrderId);
-}
--- api/order-service/src/main/java/store/order/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package store.order;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<OrderModel, String> {
-    public Iterable<OrderModel> findByIdAccount(String idAccount);
-}
